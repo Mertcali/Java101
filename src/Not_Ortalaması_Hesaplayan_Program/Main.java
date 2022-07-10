830e8495229ff206f6e17303eb7747cb5c3a5354
Mert Calis <calisahmetmert@gmail.com>
2022-07-10 15:18:52 +0300

    Initial commit

---
+++ src/Not_Ortalaması_Hesaplayan_Program/Main.java
@@ -0,0 +1,37 @@
+package Not_Ortalamasý_Hesaplayan_Program;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+			
+		int mat,fizik,kimya,turkce,tarih;
+		
+		Scanner inp = new Scanner(System.in);
+		
+		System.out.print("Matematik Notunuz: ");
+		mat= inp.nextInt();
+		
+		System.out.print("Fizik Notunuz: ");
+		fizik= inp.nextInt();
+		
+		System.out.print("Kimya Notunuz: ");
+		kimya= inp.nextInt();
+		
+		System.out.print("Türkçe Notunuz: ");
+		turkce= inp.nextInt();
+		
+		System.out.print("Tarih Notunuz: ");
+		tarih= inp.nextInt();
+		
+		double ortalama = (mat + fizik + kimya + turkce + tarih) / 5;
+		System.out.println("Ortalamanýz : " + ortalama);
+		
+		boolean checkOrtalama = ortalama >=60;
+		String gectiMi = checkOrtalama ? "Sýnýfý Geçti" : "Sýnýfta Kaldý";
+		System.out.println(gectiMi);
+
+	}
+
+}
