5afc77c45ad2352a6af4a7a93d507a3493a60780
Mert Calis <calisahmetmert@gmail.com>
2022-07-11 01:51:39 +0300

    artik_yil

---
+++ src/Artik_Yil_Programi/Main.java
@@ -0,0 +1,38 @@
+package Artik_Yil_Programi;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int artikYil;
+		boolean kosul = false;
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Yýlý giriniz: ");
+		artikYil = scanner.nextInt();
+		
+		if(artikYil%4==0) {
+			if(artikYil % 100 == 0) {
+				if(artikYil % 400 == 0) {
+					kosul = true;
+				}else {
+					kosul = false;
+				}
+			}else {
+				kosul=true;
+			}
+		}else {
+			kosul=false;
+		}
+		
+		if(kosul) {
+			System.out.println("" + artikYil + " Bir Artýk Yýldýr");
+		}else {
+			System.out.println("" +artikYil + " Bir Artýk Yýl Deðildir");
+		}
+	
+	}
+
+}
