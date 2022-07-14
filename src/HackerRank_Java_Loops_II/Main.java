81f4c306c7847dd723a10006e29d6bd05454d8cd
Mert Calis <calisahmetmert@gmail.com>
2022-07-14 18:19:00 +0300

    hackerrank

---
+++ src/HackerRank_Java_Loops_II/Main.java
@@ -0,0 +1,25 @@
+package HackerRank_Java_Loops_II;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int c = a;
+            for(int j=0;j<n;j++){
+                c += Math.pow(2, j)*b;
+                System.out.printf("%s ",c);
+            }
+            System.out.println();
+        }
+        in.close();
+
+	}
+
+}
